41bf192075ff1f6e864cab14a5424d36e1d6ed10
Alexandru Grigore <alexandru.grigore@osf-global.com>
2017-06-26 10:49:00 +0300

    Mailchimp footer

--- footer.php
+++ footer.php
@@ -50,6 +50,5 @@
 </div>
 <?php wp_footer(); ?>
 <?php do_action( 'foundationpress_before_closing_body' ); ?>
-<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us11.list-manage.com","uuid":"50f818960e36416de7808882c","lid":"66472f9208"}) })</script>
 </body>
 </html>
